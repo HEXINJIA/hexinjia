c40523b2e56e23bd70371bdbd0c520d6b249c909
HEXINJIA <632905304@qq.com>
2018-12-30 21:51:26 +0800

    新增

---
+++ src/lib/index.js
@@ -0,0 +1,42 @@
+import ToastComponet from "./h-mobile-toast.vue"
+ let Toast ={};
+Toast.install=function(Vue,options){
+    var opt={
+        duration :3000
+    }
+    for(var key in options){
+        opt[key]=options[key];
+    }
+    Vue.prototype.$toast=function(massage,option){
+        if(typeof option=="object"){
+            for(var key in option){
+                opt[key]=option[key];
+            }
+        }
+        const ToastController=Vue.extend(ToastComponet)
+        var instance=new ToastController().$mount(document.createElement("div"))
+        instance.message=massage
+        instance.visible=true;
+        document.body.appendChild(instance.$el)
+        setTimeout(()=>{
+            instance.visible=false;
+            document.body.removeChild(instance.$el)
+        },opt.duration)
+    }
+    Vue.prototype.$toast['show']=function(massage,option){
+        Vue.prototype.$toast(massage,option)
+    }
+    Vue.prototype.$toast['info']=function(){
+        Vue.prototype.$toast(massage,option)
+    }
+    Vue.prototype.$toast['error']=function(){
+        Vue.prototype.$toast(massage,option)
+    }
+    Vue.prototype.$toast['success']=function(){
+        Vue.prototype.$toast(massage,option)
+    }
+}
+if (typeof window !== 'undefined' && window.Vue) {
+    window.Vue.use(Toast);
+}
+export default Toast;
